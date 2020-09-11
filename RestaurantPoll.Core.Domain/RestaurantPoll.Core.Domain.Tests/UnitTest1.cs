4cd30667272b5cfa7130691bffa33fa8134dadae
Vinicius da Mata Pickrodt <vinipickrodt@gmail.com>
2020-09-10 21:44:51 -0300

    estrutura base do projeto

---
+++ RestaurantPoll.Core.Domain/RestaurantPoll.Core.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RestaurantPoll.Core.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
